818888e38450c186bbf634c1e4df952e3644b73a
Ana-estacio <202501054412@alunos.estacio.br>
2025-05-16 23:11:45 +0000

    reajusti nivel 2/1

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 int main() {
-
-  #include <stdio.h>
+  
+  printf("Carta1: \n");
 
     char estado1;
     char codigo1[4];//para "A01"+caracteristica numerica´0`
@@ -18,29 +18,30 @@ int main() {
     scanf( " %c", &estado1 );
 
     printf("digite o codigo1 (A01): \n ");
-    scanf("%s", codigo1);
+    scanf(" %s", codigo1);
 
     printf("didgite nome da cidade1: \n ");
-    scanf("%c", cidade1 );
+    scanf(" %c", cidade1 );
 
     printf("digite o numero de Populacao1: \n " );
-    scanf("%d", &Populacao1 );
+    scanf(" %d", &Populacao1 );
 
     printf("digite a area1: \n ");
-    scanf("%.2f", &area1);
+    scanf(" %f", &area1 );
 
     printf("digite pib1: \n ");
-    scanf("%.2f", &pib1);
+    scanf(" %.2f", &pib1);
 
     printf("digite numeroturisticos1: \n ");
-    scanf("%d", &numeroturisticos1);
+    scanf(" %d", &numeroturisticos1);
 
     printf("digite valor populacao1/area1: \n ");
-    scanf("%.2f", desidadepopulacao1 );
+    scanf(" %.2f", desidadepopulacao1 );
 
     printf("digite valor pib1/populacao1: \n ");
-    scanf("%.2f",pibpercapita1 );
-    
+    scanf(" %.2f",pibpercapita1 );
+  
+    printf("Carta2: \n");
 
     char estado2;
     char codigo2[4];//para "A01"+caracteristica numerica´0`
@@ -56,28 +57,28 @@ int main() {
     scanf( " %c", &estado2 );
 
     printf("digite o codigo2 (A01): \n ");
-    scanf("%s", codigo2);
+    scanf(" %s", codigo2);
 
     printf("didgite nome da cidade2: \n ");
-    scanf("%c",&cidade2 );
+    scanf(" %c",&cidade2 );
 
     printf("digite o numero de Populacao2: \n " );
-    scanf("%.2f",&Populacao2);
+    scanf(" %.2f",&Populacao2);
 
     printf("digite a area2: \n ");
-    scanf("%.2f", &area2);
+    scanf(" %.2f", &area2);
 
     printf("digite pib2: \n ");
-    scanf("%.2f", &pib2);
+    scanf(" %.2f", &pib2);
 
     printf("digite numeroturisticos2: \n ");
-    scanf("%d", &numeroturisticos2);
+    scanf(" %d", &numeroturisticos2);
 
     printf("digite valor populacao2/area2: \n ");
-    scanf("%.2f",desitadepopulacao2 );
+    scanf(" %.2f",desitadepopulacao2 );
 
     printf("digite valor pib2/populacao2: \n ");
-    scanf("%.2f",pibpercapita2 );
+    scanf(" %.2f",pibpercapita2 );
 
     return 0;
 }
